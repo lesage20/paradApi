ed30e588465fb7d0b83698cc2b3439427b8efa5e
lesage20 <angezanou00@gmail.com>
2022-06-01 16:55:17 +0000

    Update settings.py

--- paradApi/settings.py
+++ paradApi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-apq(z__w2vp_e3uqxq9c8dmtrhrh7orrl2cw%qjoqpm)unt551
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['paradisias-api.herokuapp.com']
 
 
 # Application definition
