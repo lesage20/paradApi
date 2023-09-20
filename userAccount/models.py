d2d0e788c4366b0c7f27a6e394e5e152798515c7
lesage20 <angezanou00@gmail.com>
2023-09-20 16:10:44 +0000

    update client to add some fields

--- userAccount/models.py
+++ userAccount/models.py
@@ -14,7 +14,10 @@ idTypes = [
     
 ]
 class Profil(models.Model):
+    # user config
+    role = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='profils', null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    # about the person
     avatar = models.ImageField(upload_to="avatars/", default="avatars/usericon.png", blank=True)
     name = models.CharField(max_length=50) 
     firstname = models.CharField(max_length=100)
@@ -23,11 +26,24 @@ class Profil(models.Model):
     email = models.CharField(max_length=250, null=True, blank=True) 
     address = models.CharField(max_length=50, null=True, blank=True)
     dob = models.DateField()
+    place_of_birth = models.CharField(max_length=50, null=True)
+    department = models.CharField(max_length=50, null=True)  
+    nationnalite = models.CharField(max_length=50, null=True)
+    job = models.CharField(max_length=50, null=True)
+    domicile = models.CharField(max_length=150, null=True)
+
+    # Id details
     idType = models.CharField(max_length=50, choices=idTypes)
     idNumber = models.CharField(max_length=20)
-    role = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='profils', null=True, blank=True)
-    department = models.CharField(max_length=50, null=True, blank=True)  
+    id_delivered_at = models.DateField(auto_now=False, auto_now_add=False)
+    id_delivered_place = models.CharField(max_length=150)
+    id_delivered_by = models.CharField(max_length=150)
+    # Parent details
+    father = models.CharField(max_length=150, null=True)
+    mother = models.CharField(max_length=150, null=True)
+
     
+
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
@@ -40,6 +56,15 @@ class Profil(models.Model):
             self.role = Group.objects.get(name="client")
         else:
             self.role = self.user.groups.all()[0]
+
+        if self.role.name == "client":
+            if any( not i for i in  [self.id_delivered_at ,self.id_delivered_place ,self.id_delivered_by ,self.father ,
+                self.mother ,self.nationnalite ,self.job ,self.domicile ]):
+                
+                raise ValueError("Données manquante", "Pour les client certaines informations sont obligatoire veuillez toutes les remplir ")
+                
+
+
         return super().save(*args, **kwargs)
     
     def __str__(self) -> str:
